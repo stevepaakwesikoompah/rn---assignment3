3b1738d2ed60ea0577e0b7b03315cbdf0c732c65
steve Koompah <koompahsteve@gmail.com>
2024-06-02 18:44:33 +0000

    category class

--- tomakeApp/Components/Categories.js
+++ tomakeApp/Components/Categories.js
@@ -1,120 +1,68 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView } from 'react-native';
+import { View, Image, TextInput, StyleSheet } from 'react-native';
 
-export default function Categories() {
+export default function SearchBar({ searchQuery, setSearchQuery }) {
   return (
-    <>
-      <View style={styles.categoriesBox}>
-        <Text style={styles.catText}>Categories</Text>
-      </View>
-      <ScrollView style={styles.scroll} horizontal={true}>
-        <View style={styles.categoriesBox1}>
-          <View style={styles.catbox1}>
-            <View style={styles.exerciseBox}>
-              <Text style={styles.exText}>Exercise</Text>
-              <Text style={styles.exTex1}>12 tasks</Text>
-              <Image style={styles.womanImage} source={require('../assets/young woman working online.png')} />
-            </View>
-          </View>
-          <View style={styles.catbox1}>
-            <View style={styles.exerciseBox}>
-              <Text style={styles.exText}>Study</Text>
-              <Text style={styles.exTex1}>12 tasks</Text>
-              <Image style={styles.womanImage} source={require('../assets/young woman working at desk.png')} />
-            </View>
-          </View>
-          <View style={styles.catbox1}>
-            <View style={styles.exerciseBox}>
-              <Text style={styles.exText}>Cook</Text>
-              <Text style={styles.exTex1}>12 tasks</Text>
-              <Image style={styles.image1} source={require('../assets/cooking.png')} />
-            </View>
-          </View>
-          <View style={styles.catbox1}>
-            <View style={styles.exerciseBox}>
-              <Text style={styles.exText}>Washing</Text>
-              <Text style={styles.exTex1}>12 tasks</Text>
-              <Image style={styles.image1} source={require('../assets/washing.png')} />
-            </View>
-          </View>
-          <View style={styles.catbox1}>
-            <View style={styles.exerciseBox}>
-              <Text style={styles.exText}>Playing Piano</Text>
-              <Text style={styles.exTex1}>12 tasks</Text>
-              <Image style={styles.image1} source={require('../assets/playing piano.png')} />
-            </View>
-          </View>
-          <View style={styles.catbox1}>
-            <View style={styles.exerciseBox}>
-              <Text style={styles.exText}>Teaching</Text>
-              <Text style={styles.exTex1}>12 tasks</Text>
-              <Image style={styles.image1} source={require('../assets/teaching.png')} />
-            </View>
-          </View>
-          <View style={styles.catbox1}>
-            <View style={styles.exerciseBox}>
-              <Text style={styles.exText}>Coding</Text>
-              <Text style={styles.exTex1}>12 tasks</Text>
-              <Image style={styles.image1} source={require('../assets/coding.png')} />
-            </View>
-          </View>
-          <View style={styles.catbox1}>
-            <View style={styles.exerciseBox}>
-              <Text style={styles.exText}>Playing Soccer</Text>
-              <Text style={styles.exTex1}>12 tasks</Text>
-              <Image style={styles.image1} source={require('../assets/playing soccer.png')} />
-            </View>
-          </View>
+    <View style={styles.searchInGeneral}>
+      <View style={styles.searchEngine}>
+        <View style={styles.searchInput}>
+          <Image source={require('../assets/Vector.png')} style={styles.searchIcon} />
+          <TextInput
+            style={styles.minisearchInput}
+            placeholder='Search'
+            value={searchQuery}
+            onChangeText={text => setSearchQuery(text)}
+          />
         </View>
-      </ScrollView>
-    </>
+      </View>
+      <View style={styles.searchImage}>
+        <Image source={require('../assets/Vector_1.png')} style={styles.filterImage} />
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  categoriesBox: {
-    marginHorizontal: 20,
-    marginTop: 29,
-  },
-  catText: {
-    fontSize: 20,
-    fontWeight: '700',
-  },
-  categoriesBox1: {
+  searchInGeneral: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginHorizontal: 20,
-    marginTop: 6,
-  },
-  catbox1: {
-    width: 150,
-    height: 192,
-    backgroundColor: 'white',
-    borderRadius: 15,
-    marginRight: 10,
-  },
-  exerciseBox: {
-    padding: 10,
-  },
-  exText: {
-    fontSize: 16,
-    fontWeight: '700',
-  },
-  exTex1: {
-    fontSize: 12,
-    fontWeight: '400',
-  },
-  womanImage: {
-    width: 150,
-    height: 132,
     marginTop: 10,
   },
-  image1:{
-    width: 130,
-    height:120,
-    
+  searchEngine: {
+    flex: 1,
+    height: 48,
+    backgroundColor: 'white',
+    borderRadius: 14,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  scroll: {
-    height: 550,
+  searchInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 6,
+  },
+  searchIcon: {
+    width: 24,
+    height: 24,
+  },
+  minisearchInput: {
+    flex: 1,
+    height: '100%',
+    paddingHorizontal: 8,
+    fontSize: 16,
+  },
+  searchImage: {
+    width: 50,
+    height: 48,
+    backgroundColor: '#F0522F',
+    borderRadius: 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 8,
+  },
+  filterImage: {
+    width: 28.3,
+    height: 25.5,
   },
 });
